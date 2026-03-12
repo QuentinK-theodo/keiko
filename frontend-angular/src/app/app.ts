6ae0708671043880ae79ad87ab6265c51a7427fc
QuentinK-theodo <quentin.krempp@theodo.com>
2026-03-12 10:19:31 +0100

    Splitted frontend between 'frontend-react' and 'frontend-angular', added Carapuce component + tests

---
+++ frontend-angular/src/app/app.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'pokemon',
+  template: `
+  <div class=.intro>
+    <div>Bienvenue sur ton futur pokédex !</div>
+      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="Carapuce" />
+        <p>{{ name }}</p>
+        <p>Number: 7</p>
+      </div>
+  </div>
+  `,
+})
+export class Pokemon {
+  name = 'Carapuce';
+  number = 7;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Pokemon],
+  // templateUrl: './app.html',
+  template: `
+    <pokemon />
+  `,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend-angular');
+}
+
+
