d8ae9231bf449bc07011826e8303812ba9a3d520
QuentinK-theodo <quentin.krempp@theodo.com>
2026-03-12 10:19:31 +0100

    Splitted frontend between 'frontend-react' and 'frontend-angular', added Carapuce component + tests

---
+++ frontend-angular/src/app/app.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { App, Pokemon } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should display carapuce', async () => {
+    const fixture = TestBed.createComponent(Pokemon);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('div')?.textContent).toContain('Carapuce');
+  });
+
+});
