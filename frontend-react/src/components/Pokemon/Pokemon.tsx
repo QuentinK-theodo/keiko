983c75bece5175a90fc489b4b55d639a388ed812
QuentinK-theodo <quentin.krempp@theodo.com>
2026-03-12 11:59:20 +0100

    Created separated Pokemon component

---
+++ frontend-react/src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,13 @@
+export const Pokemon = () => {
+  return (
+    <div>
+      <div>Bienvenue sur ton futur pokédex !</div>
+      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="Carapuce" />
+        <p>Carapuce</p>
+        <p>Number: 7</p>
+      </div>
+    </div>
+  )
+}
