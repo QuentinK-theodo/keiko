a80641234ab1300c94569adb2a51ae2e9f7e678a
QuentinK-theodo <quentin.krempp@theodo.com>
2026-03-16 10:09:45 +0100

    Added loader

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,39 +1,47 @@
 import { Component, signal, inject, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { Pokemon } from './pokemon/pokemon';
 import { PokemonInfo } from './pokemon';
 import { PokemonService } from './pokemon-service';
 
+import { Loader } from './loader/loader';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Pokemon],
+  imports: [RouterOutlet, Pokemon, Loader],
   template: `
   <div>
     <div class="intro">
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur Angular et attraper des pokemons !</div>
     </div>
-
-    <div class="container">
-      @for (pokemonInfo of filteredPokemonInfoList; track $index){
+    @if (isLoading) {
+      <app-loader />
+    } @else {
+      <div class="container">
+      @for (pokemonInfo of pokemonInfoList; track $index){
         <pokemon [pokemonInfo] = "pokemonInfo"/>
         }
-    </div>
+      </div>
+    }
   </div>
   `,
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('frontend-angular');
+
   private readonly changeDetectorRef = inject(ChangeDetectorRef)
   pokemonInfoList: PokemonInfo[] = []
-  filteredPokemonInfoList: PokemonInfo[] = []
   pokemonService: PokemonService = inject(PokemonService)
+  
+  isLoading: boolean = true
 
   ngOnInit() {
     this.pokemonService.getFullPokemonList().then((pokemonInfoList: PokemonInfo[]) => {
       this.pokemonInfoList = pokemonInfoList
-      this.filteredPokemonInfoList = pokemonInfoList;
+      this.isLoading = false
       this.changeDetectorRef.markForCheck();
     })
   }
